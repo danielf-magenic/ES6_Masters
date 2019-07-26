3d19deec707bc4820e6bcfc37743b3c168743d01
Daniel Fabia <danief@magenic.com>
2019-07-27 01:05:49 +0800

    Added Exercise1

---
+++ Exercise1/Exercise1.js
@@ -0,0 +1,44 @@
+var input = "The quick brown QA jumps over the sleeping Developer";
+//var input = "bcdfghjklmnpqrstvwxyzbcdfghjklmnpqrstvwxyzbcdfghjklmnpqrstvwxyz";
+//var input= "aeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiou";
+
+if (input.length >= 50) {
+    var wordCount = countWords(input);
+    var consonantCount = count(input, /[bcdfghjklmnpqrstvwxyz]/);
+    var vowelCount = count(input, /[aeiou]/);
+
+    var out = theTag`Input: ${input}
+ Number of word(s) found: ${wordCount}
+ Number of consonant(s) found: ${consonantCount}
+ Number of vowel(s) found: ${vowelCount}`;
+
+    console.log(out);
+}
+else
+    console.log("Length must be greater than 50.");
+
+
+
+function theTag(strings, input, wc, cc, vc) {
+    var [i, w, c, v] = strings;
+    var s = `${i}${input}${w}${wc}${cc != 0 ? `${c}${cc}` : ""}${vc != 0 ? `${v}${vc}` : ""}`;
+
+    return s;
+}
+
+function countWords(input) {
+    return input.split(' ').length;
+}
+
+function count(input, expression) {
+    var str = input.toLowerCase();
+    var count = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        var char = str.charAt(i);
+        if (char.match(expression))
+            count++;
+    }
+
+    return count;
+}
